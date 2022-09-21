14760150ae0dae5e487b909e82dda3e7bef41793
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2022-09-21 12:01:22 -0400

    first pager version

--- src/Pager/Pager.js
+++ src/Pager/Pager.js
@@ -1,7 +1,13 @@
 import "./Pager.css";
+import Panel from "components/Panel/Panel";
+import PagerComp from "components/Pager/PagerComp";
 
 const Pager = () => {
-  return null;
+  return(
+    <Panel>
+      <PagerComp></PagerComp>
+    </Panel>
+  )
 };
 
 export default Pager;
